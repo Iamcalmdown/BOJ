5cbfc69b156ae2b6f53cd9aa6483f0731b36bd37
DongHyoen Jeong <144317474+Iamcalmdown@users.noreply.github.com>
2024-12-07 17:06:10 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -5,11 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        long sum = 0;
-        for (int i = 1; i <= n - 2; i++) {
-            sum += (long) (n - i) * (n - i - 1) / 2;
-        }
-        System.out.println(sum + "\n3");
+//        int n = Integer.parseInt(br.readLine());
+//        long sum = 0;
+//        for (int i = 1; i <= n - 2; i++) {
+//            sum += (long) (n - i) * (n - i - 1) / 2;
+//        }
+//        System.out.println(sum + "\n3");
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n * (n - 1) * (n - 2) / 6 + "\n3");
+
     }
 }
