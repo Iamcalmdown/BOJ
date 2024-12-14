f79ad3d2d57a795126fb592a68b8e7463a03b91e
DongHyoen Jeong <144317474+Iamcalmdown@users.noreply.github.com>
2024-12-14 17:54:07 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 11512 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,14 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int M = 1234567891;
         int L = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        long sum = 0;
+        long sum = 0, pow = 1;
         for (int i = 0; i < L; i++) {
-            sum += (long) (str.charAt(i) - 96) * (int) (Math.pow(31, i));
+            sum += (str.charAt(i) - 96) * pow;
+            pow = (pow * 31) % M;
         }
-        System.out.println(sum);
+        System.out.println(sum % M);
     }
 }
