4244d63aceb79fc5de047865c50a62c57e8e99bd
DongHyoen Jeong <144317474+Iamcalmdown@users.noreply.github.com>
2025-01-02 03:25:29 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 72 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N]; // 현재 줄 선 사람들 번호표
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -17,29 +16,25 @@ public class Main {
         }
         System.out.println(solve(arr));
     }
-
     public static String solve(int[] def) {
-        int[] stack = new int[N];
-        int num = 1, size = 0; // 처음에 비교해서 출력할 번호 표 값과 스택의 크기
-
+        Stack<Integer> stack = new Stack<>();
+        int num = 1;
         for (int i = 0; i < def.length; i++) {
             if (num == def[i]) {
                 num++;
             } else {
-                while (size != 0 && stack[size - 1] == num) {
+                while (!stack.empty() && stack.peek() == num) {
+                    stack.pop();
                     num++;
-                    size--;
                 }
-                stack[size] = def[i];
-                size++;
+                stack.push(def[i]);
             }
         }
-        for (int i = size - 1; i >= 0; i--) {
-            if (num == stack[i]) {
-                num++;
-            }
+        while (!stack.empty() && stack.peek() == num) {
+            stack.pop();
+            num++;
         }
-        if (num - 1 == N) {
+        if (stack.empty()) {
             return "Nice";
         } else {
             return "Sad";
