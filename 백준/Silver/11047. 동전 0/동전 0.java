c288ff45d72e20087d244db0a7d49c3c3eff6a07
DongHyoen Jeong <144317474+Iamcalmdown@users.noreply.github.com>
2025-01-19 15:36:45 +0900

    [Silver IV] Title: 동전 0, Time: 60 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -7,18 +7,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] coin = new int[N];
-        for (int i = N - 1; i >= 0; i--) {
-            coin[i] = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
         int sum = 0;
-        for (int num : coin) {
-            sum += K / num;
-            K %= num;
+        for (int i = N - 1; i >= 0; i--) {
+            if (K >= arr[i]) {
+                sum += (K / arr[i]);
+                K %= arr[i];
+            }
         }
-        System.out.print(sum);
+        System.out.println(sum);
     }
 }
